d093cb1f7441ab0fd9f82ea28c5878d824c4ada6
Niklas Modess <niklas@modess.io>
2015-11-21 13:24:07 +0800

    create simple page views table

---
+++ app/DoctrineMigrations/Version20151121041532.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151121041532 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->createTable('page_views');
+        $table->addColumn('ip', 'string');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $schema->dropTable('users');
+    }
+}
