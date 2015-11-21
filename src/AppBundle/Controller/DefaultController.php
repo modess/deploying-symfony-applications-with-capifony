0cd1781b018cc16a86a0ad0211d6a64692e7c2f2
Niklas Modess <niklas@modess.io>
2015-11-21 13:24:16 +0800

    save, fetch and display page views

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -24,10 +24,22 @@ class DefaultController extends Controller
             $latestDeploy = $latestDeploy->format('r');
         }
 
+        // Get doctrine manager
+        $em = $this->getDoctrine()->getManager();
+
+        // Write page view to database, the ugly way
+        $sql = "INSERT INTO page_views SET ip = '" . $this->get('request')->getClientIp() . "'";
+        $em->getConnection()->exec($sql);
+
+        // Write page view to database, the ugly way
+        $sql       = "SELECT count(*) as cnt FROM page_views";
+        $pageViews = $em->getConnection()->query($sql)->fetchAll(\PDO::FETCH_COLUMN)[0];
+
         return $this->render('default/index.html.twig', array(
             'first_deploy'  => $firstDeploy,
             'latest_deploy' => $latestDeploy,
             'life_span'     => $lifeSpan,
+            'page_views'    => $pageViews
         ));
     }
 }
