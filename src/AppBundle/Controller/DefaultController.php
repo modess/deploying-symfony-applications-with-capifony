c9c9be134e702c1b11636ba73d99e2bd8fc35b06
Niklas Modess <niklas@modess.io>
2015-11-23 09:17:18 +0800

    only count page views when deployed

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -17,23 +17,24 @@ class DefaultController extends Controller
         $firstDeploy  = $this->get('cache')->fetch('first_deploy');
         $latestDeploy = $this->get('cache')->fetch('latest_deploy');
         $lifeSpan     = null;
+        $pageViews    = 0;
 
         if ($firstDeploy !== false) {
             $firstDeploy  = $firstDeploy->format('r');
             $lifeSpan     = (new Carbon($firstDeploy))->diffForHumans(Carbon::now(), true);
             $latestDeploy = $latestDeploy->format('r');
+
+            // Get doctrine manager
+            $em = $this->getDoctrine()->getManager();
+
+            // Write page view to database, the ugly way
+            $sql = "INSERT INTO page_views SET ip = '" . $this->get('request')->getClientIp() . "'";
+            $em->getConnection()->exec($sql);
+
+            // Write page view to database, the ugly way
+            $sql       = "SELECT count(*) as cnt FROM page_views";
+            $pageViews = $em->getConnection()->query($sql)->fetchAll(\PDO::FETCH_COLUMN)[0];
         }
-
-        // Get doctrine manager
-        $em = $this->getDoctrine()->getManager();
-
-        // Write page view to database, the ugly way
-        $sql = "INSERT INTO page_views SET ip = '" . $this->get('request')->getClientIp() . "'";
-        $em->getConnection()->exec($sql);
-
-        // Write page view to database, the ugly way
-        $sql       = "SELECT count(*) as cnt FROM page_views";
-        $pageViews = $em->getConnection()->query($sql)->fetchAll(\PDO::FETCH_COLUMN)[0];
 
         return $this->render('default/index.html.twig', array(
             'first_deploy'  => $firstDeploy,
